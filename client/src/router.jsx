b8a70c73d20b111e9708b79269978f346baa144b
Alex Menendez <alexis.menendez@austincc.edu>
2025-04-08 10:26:03 -0500

    Update router.jsx

---
+++ client/src/router.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { ApolloProvider } from '@apollo/client';
+import App from './App';
+import './styles/index.css'; // Tailwind base styles
+import client from './graphql/client'; // Apollo client setup
+import AuthProvider from './context/AuthProvider'; // Your custom context
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ApolloProvider client={client}>
+      <AuthProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </AuthProvider>
+    </ApolloProvider>
+  </React.StrictMode>
+);
