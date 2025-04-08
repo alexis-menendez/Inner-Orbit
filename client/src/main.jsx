bfc25c9f948d2159291b420517c6228c6288f40b
Alex Menendez <alexis.menendez@austincc.edu>
2025-04-08 10:26:25 -0500

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,20 @@
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { ApolloProvider } from '@apollo/client';
+import App from './App';
+import './styles/index.css'; // Tailwind base styles
+import client from './graphql/client'; // Apollo client setup
+import AuthProvider from './context/AuthProvider'; // Your custom context
 
-const client = new ApolloClient({
-  uri: '/graphql',
-  cache: new InMemoryCache(),
-});
-
-<ApolloProvider client={client}>
-  <App />
-</ApolloProvider>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ApolloProvider client={client}>
+      <AuthProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </AuthProvider>
+    </ApolloProvider>
+  </React.StrictMode>
+);
