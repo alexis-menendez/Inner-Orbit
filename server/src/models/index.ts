42f082882bf6a30acdec276a2e537c1fcd4fb77c
Halluc1nations <Blaine.Bishop@comcast.net>
2025-05-10 23:51:11 -0400

    updated typedefs

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -1,7 +1,7 @@
 // File: server/src/models/index.ts
 
-import User from './User';
-import MoodEntry from './Tracker';
+import User from './User.js';
+import MoodEntry from './Tracker.js';
 //import JournalEntry from './Journal'; uncomment this line once Blake has created the Journal model/Alexis
 import Video from './Video.js';
 
--- server/src/schema/typeDefs.ts
+++ server/src/schema/typeDefs.ts
@@ -1,15 +1,32 @@
 // File: server/src/schema/typeDefs.ts
 
-import gql from 'graphql-tag'; 
-
+import gql from "graphql-tag";
 
 const typeDefs = gql`
-   type User {
+  scalar Date
+
+  type User {
     _id: ID!
     username: String!
     email: String!
     moodEntries: [MoodEntry]
-    //journalEntries: [JournalEntry]
+    journalEntries: [JournalEntry]
+  }
+
+  type MoodEntry {
+    _id: ID!
+    mood: String!
+    color: String!
+    createdAt: Date!
+    user: User!
+  }
+
+  type JournalEntry {
+    _id: ID!
+    title: String!
+    content: String!
+    createdAt: Date!
+    user: User!
   }
 
   type AuthPayload {
@@ -21,21 +38,39 @@ const typeDefs = gql`
     getUserById(userId: ID!): User
     me: User
     getMoodEntries: [MoodEntry]
-    //getJournalEntries: [JournalEntry]
+    getJournalEntries: [JournalEntry]
   }
 
   type Mutation {
-    registerUser(username: String!, email: String!, password: String!): AuthPayload
+    registerUser(
+      username: String!
+      email: String!
+      password: String!
+    ): AuthPayload
     loginUser(username: String!, password: String!): AuthPayload
 
     addMoodEntry(mood: String!, intensity: Int!, color: String!): MoodEntry
-    //addJournalEntry(title: String!, content: String!, videoUrl: String): JournalEntry
+    addJournalEntry(
+      title: String!
+      content: String!
+      videoUrl: String
+    ): JournalEntry
 
-    updateMoodEntry(id: ID!, mood: String, intensity: Int, color: String): MoodEntry
-    //updateJournalEntry(id: ID!, title: String, content: String, videoUrl: String): JournalEntry
+    updateMoodEntry(
+      id: ID!
+      mood: String
+      intensity: Int
+      color: String
+    ): MoodEntry
+    updateJournalEntry(
+      id: ID!
+      title: String
+      content: String
+      videoUrl: String
+    ): JournalEntry
 
     deleteMoodEntry(id: ID!): Boolean
-    //deleteJournalEntry(id: ID!): Boolean
+    deleteJournalEntry(id: ID!): Boolean
   }
 `;
 
