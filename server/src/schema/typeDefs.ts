f331fb2a52ad70fa6e9ec517d8a36250791a0535
Halluc1nations <Blaine.Bishop@comcast.net>
2025-05-10 22:49:30 -0400

    typedefs & resolvers for tracker

--- server/src/schema/typeDefs.ts
+++ server/src/schema/typeDefs.ts
@@ -4,25 +4,38 @@ import gql from 'graphql-tag';
 
 
 const typeDefs = gql`
-  type User {
+   type User {
     _id: ID!
     username: String!
     email: String!
-    isDev: Boolean
-    createdAt: String
+    moodEntries: [MoodEntry]
+    //journalEntries: [JournalEntry]
   }
 
   type AuthPayload {
     token: String!
+    user: User!
   }
 
   type Query {
     getUserById(userId: ID!): User
+    me: User
+    getMoodEntries: [MoodEntry]
+    //getJournalEntries: [JournalEntry]
   }
 
   type Mutation {
     registerUser(username: String!, email: String!, password: String!): AuthPayload
     loginUser(username: String!, password: String!): AuthPayload
+
+    addMoodEntry(mood: String!, intensity: Int!, color: String!): MoodEntry
+    //addJournalEntry(title: String!, content: String!, videoUrl: String): JournalEntry
+
+    updateMoodEntry(id: ID!, mood: String, intensity: Int, color: String): MoodEntry
+    //updateJournalEntry(id: ID!, title: String, content: String, videoUrl: String): JournalEntry
+
+    deleteMoodEntry(id: ID!): Boolean
+    //deleteJournalEntry(id: ID!): Boolean
   }
 `;
 
