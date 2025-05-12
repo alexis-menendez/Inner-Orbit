5e66f78692b1c26c3c97fc6b383d98c7784a7040
Alex Menendez <alexis.menendez@austincc.edu>
2025-05-12 15:02:51 -0500

    Update index.ts

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -2,7 +2,7 @@
 
 import User from './User.js';
 import MoodEntry from './Tracker.js';
-import JournalEntry from './Journal';
+import JournalEntry from './Journal.js';
 import Video from './Video.js';
 
 export {
