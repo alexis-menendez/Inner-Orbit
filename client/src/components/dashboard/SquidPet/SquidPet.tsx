28880d32140bbc1371fbbbc448f5ecf3db38931b
Alexis Menendez <alexis.menendez@austincc.edu>
2025-05-21 09:57:46 -0500

    cerated squid pet placeholder so that I could boot up

---
+++ client/src/components/dashboard/SquidPet/SquidPet.tsx
@@ -0,0 +1,13 @@
+// File: client/src/components/dashboard/SquidPet/SquidPet.tsx
+
+import React from 'react';
+
+const SquidPet: React.FC = () => {
+  return (
+    <div>
+      <p>🐙 SquidPet component placeholder</p>
+    </div>
+  );
+};
+
+export default SquidPet;
