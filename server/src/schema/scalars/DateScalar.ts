6eefe23ac166515a320710a43aade9acff4a9ab7
Alexis Menendez <alexis.menendez@austincc.edu>
2025-05-21 10:17:05 -0500

    added error log on date scalar

--- server/src/schema/scalars/DateScalar.ts
+++ server/src/schema/scalars/DateScalar.ts
@@ -6,8 +6,11 @@ const DateScalar = new GraphQLScalarType({
   name: 'Date',
   description: 'Custom scalar for Date values',
   serialize(value) {
-    return value instanceof Date ? value.toISOString() : null;
-  },
+  if (!(value instanceof Date)) {
+    throw new TypeError("Value is not a Date");
+  }
+  return value.toISOString();
+},
   parseValue(value) {
     return typeof value === 'string' ? new Date(value) : null;
   },
