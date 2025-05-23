0ed86f1a199653968b6ff3358d8da096ab3040a4
Alexis Menendez <alexis.menendez@austincc.edu>
2025-05-23 00:04:55 -0500

    Refactored Tracker typeDefs
    I'm just going to have to completely rework the tracker I think. The journal works, going to model it off of that

--- server/src/schema/typeDefs.ts
+++ server/src/schema/typeDefs.ts
@@ -6,6 +6,13 @@ const typeDefs = gql`
 
   scalar Date
 
+  type AuthPayload {
+    token: String!
+    user: User!
+    success: Boolean
+    message: String
+  }
+
   type User {
     _id: ID!
     username: String!
@@ -19,48 +26,56 @@ const typeDefs = gql`
 
   type MoodEntry {
     _id: ID!
+    userId: ID!
     mood: String!
     moodColor: String!
     intensity: Int!
     date: Date!
     createdAt: Date!
-    user: User!
     note: String
   }
 
-  type MoodByDate {
-    date: String!
+  type CreateMoodPayload {
+    success: Boolean!
+    message: String
+    entry: MoodEntry
+  }
+
+  type UpdateMoodPayload {
+    success: Boolean!
+    message: String
+    entry: MoodEntry
+  }
+
+  type GetMoodEntriesPayload {
+    success: Boolean!
+    message: String
+    entries: [MoodEntry!]!
+  }
+
+  input CreateMoodInput {
+    userId: ID!
+    date: Date!
     mood: String!
+    intensity: Int!
+    moodColor: String!
     note: String
   }
 
-  type JournalEntry {
+  input UpdateMoodInput {
+    mood: String
+    intensity: Int
+    moodColor: String
+    note: String
+  }
+
+    type JournalEntry {
     _id: ID!
+    userId: ID!
     title: String!
     content: String!
     mood: String
     createdAt: Date!
-    userId: ID!
-  }
-
-  type AuthPayload {
-    token: String!
-    user: User!
-    success: Boolean
-    message: String
-  }
-
-  input CreateJournalInput {
-    userId: ID!
-    title: String!
-    content: String!
-    mood: String
-  }
-
-  input UpdateJournalInput {
-    title: String
-    content: String
-    mood: String
   }
 
   type CreateJournalPayload {
@@ -87,58 +102,66 @@ const typeDefs = gql`
     message: String
   }
 
+  input CreateJournalInput {
+    userId: ID!
+    title: String!
+    content: String!
+    mood: String
+  }
+
+  input UpdateJournalInput {
+    title: String
+    content: String
+    mood: String
+  }
+
   type Query {
     getUserById(userId: ID!): User
-    getJournalEntryById(entryId: ID!): JournalEntry
-    getMoodEntryByDate(userId: ID!, date: Date!): MoodEntry
-    getMoodEntriesByDateRange(startDate: Date!, endDate: Date!): [MoodEntry]
-    getMoodEntries(userId: ID!): [MoodEntry]
-    getJournalEntries(userId: ID!): GetJournalEntriesPayload!
-    moodsByDates(dates: [String!]!): [MoodEntry]
     me: User
   }
 
   extend type Query {
+    getMoodEntryByDate(userId: ID!, date: Date!): MoodEntry
+    getMoodEntriesByDateRange(startDate: Date!, endDate: Date!): [MoodEntry]
+    getMoodEntries(userId: ID!): [MoodEntry]
+    moodsByDates(userId: ID!, dates: [String!]!): [MoodEntry]
+  }
+
+  extend type Query {
+    getJournalEntries(userId: ID!): GetJournalEntriesPayload!
+    getJournalEntryById(entryId: ID!): JournalEntry  
     getCompletedConstellations(userId: ID!): CompletedConstellationsPayload!
   }
 
   type Mutation {
-    registerUser(
-      username: String!
-      firstName: String
-      lastName: String
-      dob: String
-      email: String!
-      password: String!
-    ): AuthPayload
+      registerUser(
+        username: String!
+        firstName: String
+        lastName: String
+        dob: String
+        email: String!
+        password: String!
+      ): AuthPayload
 
-    loginUser(username: String!, password: String!): AuthPayload
+      loginUser(
+        username: String!
+        password: String!
+      ): AuthPayload
+    }
 
-        addMoodEntry(
-      date: Date!
-      mood: String!
-      intensity: Int!
-      moodColor: String!
-      note: String
-      userId: ID!
-    ): MoodEntry!
+    extend type Mutation {
+      addMoodEntry(input: CreateMoodInput!): CreateMoodPayload!
+      updateMoodEntry(id: ID!, input: UpdateMoodInput!): UpdateMoodPayload!
+      deleteMoodEntry(id: ID!): Boolean
+    }
 
-    updateMoodEntry(
-      id: ID!
-      mood: String
-      intensity: Int
-      moodColor: String
-      note: String
-    ): MoodEntry
-    
-    deleteMoodEntry(id: ID!): Boolean
+    extend type Mutation {
+      createJournal(input: CreateJournalInput!): CreateJournalPayload!
+      updateJournal(input: UpdateJournalInput!): UpdateJournalPayload!
+      updateJournalEntry(id: ID!, input: UpdateJournalInput!): CreateJournalPayload!
+      deleteJournalEntry(id: ID!): Boolean
+    }
 
-    createJournal(input: CreateJournalInput!): CreateJournalPayload!
-    updateJournal(input: UpdateJournalInput!): UpdateJournalPayload!
-    updateJournalEntry(id: ID!, input: UpdateJournalInput!): CreateJournalPayload!
-    deleteJournalEntry(id: ID!): Boolean
-  }
-
-`;
+  `;
 
 export default typeDefs;
