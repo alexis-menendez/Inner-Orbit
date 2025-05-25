83d5577c33dbe56973416754804fa12797562f8b
Alexis Menendez <alexis.menendez@austincc.edu>
2025-05-25 17:04:59 -0500

    updated navbar to look good on small screens

--- client/src/components/layout/MainLayout.tsx
+++ client/src/components/layout/MainLayout.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import StarBackground from '../common/StarBackground';
-import MainLayoutBackground from '../common/MainLayoutBackground'; // NEW
+import MainLayoutBackground from '../common/MainLayoutBackground';
 import NavBar from '../nav/NavBar';
 import Footer from '../nav/Footer';
-import styles from '../../assets/css/common/CosmicBackground.module.css';
+import styles from '../../assets/css/common/MainLayoutBackground.module.css'; // ✅ Correct import
 
 const MainLayout: React.FC = () => {
   return (
     <div className="w-full min-h-screen bg-[#0a0029] text-white relative overflow-hidden">
       <StarBackground />
-      <MainLayoutBackground /> {/* INSERTED HERE */}
+      <MainLayoutBackground />
 
       <div className="relative z-10 flex flex-col items-center min-h-screen">
         <div className={styles['gradient-overlay']}>
